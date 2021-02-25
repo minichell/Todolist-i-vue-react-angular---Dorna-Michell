79a35a67e01b9a8207a36ae366bdef7445682840
minichell <54796879+minichell@users.noreply.github.com>
2021-02-25 13:11:55 +0100

    klaar

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 const app = new Vue ({
-el: '#app'
+el: '#app',
+data: {
+    title: 'Hellow Coding',
+    newTodo: '',
+    todos: []
+},
+methods:{
+    addTodo() {
+       this.todos.push({
+           title: this.newTodo,
+           done: false
+       });
+       this.newTodo = '';
+    },
+    removeTodo(todo){
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex, 1);
+    },
+    allDone(){
+        this.todos.forEach(todo =>{
+            todo.done = true;
+        });
+    }
+}
 });
